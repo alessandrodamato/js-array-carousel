4efb8e03fc416ca4a42331a64bdb8f3ab3801d76
alessandrodamato <alessandro042004@gmail.com>
2024-02-26 15:12:27 +0100

    [add] dynamic carousel (with bugs)

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,29 @@ const images = [
 let counter = 0;
 
 const slider = document.querySelector('.slider');
-const imgCollection = document.getElementsByClassName('img');
 
 for (let i = 0; i < images.length; i++) {
   const img = images[i];
   slider.innerHTML += `<img src="${img}" class="img hide">` 
 }
 
+const imgCollection = document.getElementsByClassName('img');
 imgCollection[counter].classList.remove('hide');
+
+// bottone UP
+const topButton = document.querySelector('.btn-top');
+
+topButton.addEventListener('click', function(){
+  imgCollection[counter].classList.add('hide');
+  counter--;
+  imgCollection[counter].classList.remove('hide');
+})
+
+// bottone DOWN
+const bottomButton = document.querySelector('.btn-bottom');
+
+bottomButton.addEventListener('click', function(){
+  imgCollection[counter].classList.add('hide');
+  counter++;
+  imgCollection[counter].classList.remove('hide');
+})
